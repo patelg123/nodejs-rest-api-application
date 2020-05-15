852e33edb6dbe9ceb28273f46a6e733ac2a4d63a
patelg123 <patelg123@gmail.com>
2020-05-15 13:07:51 +0100

    Added routes and corresponding Controller functions

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -9,8 +9,29 @@ const PORT = process.env.PORT || 8080;
 class UserController{
 
 	public serverTest = (req: Request, res: Response ): void => {
-		res.status(200).send(`Server is now running on port ${PORT}`)		
+		res.status(200).send(`Server is now running on port ${PORT}`);
 	}
+
+	public getUserList = (req: Request, res: Response): void => {
+		res.status(200).send(`List of users`);
+	}
+
+	public getUserDetails = (req: Request, res: Response): void => {
+		res.status(200).send(`Get details for ${req.params.userId}`);
+	}
+
+	public createUser = (req: Request, res: Response): void => {
+		res.status(200).send(`Create User`);
+	}
+	
+	public updateUser = (req: Request, res: Response): void => {
+		res.status(200).send(`Update ${req.params.userId}`)
+	}
+
+	public deleteUser = (req: Request, res: Response): void => {
+		res.status(200).send(`Delete user ${req.params.userId}`)
+	}
+	
 }
 
 export const userController = new UserController;
--- src/routes/appRoutes.ts
+++ src/routes/appRoutes.ts
@@ -10,6 +10,11 @@ class AppRoutes {
 
 	private config(): void {
 		this.router.get("/", userController.serverTest);
+		this.router.get("/users", userController.getUserList);
+		this.router.get("/users/:userId", userController.getUserDetails);
+		this.router.post("/users", userController.createUser);
+		this.router.put("/users/:userId", userController.updateUser);
+		this.router.delete("/users/:userId", userController.deleteUser);
 	}
 }
 
