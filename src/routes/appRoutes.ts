b0ac1b49e5806ee7c94c2a2c70c9db48e3a0d456
patelg123 <patelg123@gmail.com>
2020-05-15 13:28:38 +0100

    formatted code

--- src/routes/appRoutes.ts
+++ src/routes/appRoutes.ts
@@ -2,20 +2,20 @@ import * as express from "express";
 import { userController } from "../controllers/userController";
 
 class AppRoutes {
-	public router: express.Router = express.Router();
+  public router: express.Router = express.Router();
 
-	constructor() {
-		this.config();
-	}
-
-	private config(): void {
-		this.router.get("/", userController.serverTest);
-		this.router.get("/users", userController.getUserList);
-		this.router.get("/users/:userId", userController.getUserDetails);
-		this.router.post("/users", userController.createUser);
-		this.router.put("/users/:userId", userController.updateUser);
-		this.router.delete("/users/:userId", userController.deleteUser);
-	}
+  constructor() {
+    this.config();
+  }
+  
+  private config(): void {
+    this.router.get("/", userController.serverTest);
+    this.router.get("/users", userController.getUserList);
+    this.router.get("/users/:userId", userController.getUserDetails);
+    this.router.post("/users", userController.createUser);
+    this.router.put("/users/:userId", userController.updateUser);
+    this.router.delete("/users/:userId", userController.deleteUser);
+  }
 }
 
 export const appRoutes = new AppRoutes().router;
